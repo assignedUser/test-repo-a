53acc119ee05b6d1ecb7efd7fa5957054bb7f17b
Jacob Wujciak-Jens <jacob@wujciak.de>
2022-05-11 14:12:46 +0200

    internet

--- fix-keychain.sh
+++ fix-keychain.sh
@@ -2,4 +2,4 @@ security create-keychain -p  $(echo -n $.GITHUB_TOKEN | shasum | awk '{print $1}
 security set-keychain-settings webdav.keychain
 security default-keychain -s webdav.keychain
 security unlock-keychain -p $(echo -n $GITHUB_TOKEN | shasum | awk '{print $1}') webdav.keychain
-security add-account-password -U -A -P 8080 -r https -a $ASF_USER -s nightly.wujciak.de -w $ASF_KEY
+security add-internet-password -U -A -P 8080 -r https -a $ASF_USER -s nightly.wujciak.de -w $ASF_KEY
